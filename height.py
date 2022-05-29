92c0a6091294c622aeea053daca548587eede1ca
Shoaib <54897681+shoaib-se@users.noreply.github.com>
2022-05-29 15:51:24 +0000

    Initial commit

---
+++ height.py
@@ -0,0 +1,6 @@
+print("Welcome to the rollercoaster")
+height = int(input("What is your height in CM?"))
+if height == 120:
+  print("You can ride the rollercoaster!")
+else:
+  print("Soory! You have to grow taller before ride to rolllercoaster")
