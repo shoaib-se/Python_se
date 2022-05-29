29b58cf1a27cb742a5c50762200fab0b43467711
Shoaib <54897681+shoaib-se@users.noreply.github.com>
2022-05-29 16:06:29 +0000

    Initial commit

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-print("Welcome to the rollercoaster")
-height = int(input("What is your height in CM?"))
-if height == 120:
-  print("You can ride the rollercoaster!")
-else:
-  print("Soory! You have to grow taller before ride to rolllercoaster")
+name = input("What is your name?")
+length = len(name)
+print(length)
